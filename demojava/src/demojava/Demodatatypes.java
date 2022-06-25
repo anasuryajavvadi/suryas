13e70586fadcebe5c713d370fc69dff8eb914ba0
javva <javva@ANASURYA.bbrouter>
2022-06-25 16:00:59 +0530

    datatypes

---
+++ demojava/src/demojava/Demodatatypes.java
@@ -0,0 +1,17 @@
+package demojava;
+
+public class Demodatatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int max = 2147;
+int min = -2147;
+short shortMax = 32767;
+short shortmin = -32767;
+long longMax = 92233L;
+long longMin = -92233L;
+byte byteMax = 127;
+byte byteMin = -128;
+	}
+
+}
