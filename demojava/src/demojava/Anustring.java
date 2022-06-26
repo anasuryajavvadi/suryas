f384070a830bc64d6fc3e4b611ab06d7befe59de
javva <javva@ANASURYA>
2022-06-26 21:43:49 +0530

    string

---
+++ demojava/src/demojava/Anustring.java
@@ -0,0 +1,11 @@
+package demojava;
+
+public class Anustring {
+
+	public static void main(string[] args) {
+		// TODO Auto-generated method stub
+String var = new Stringtring("I Love Nature");
+System.out.println(var);
+	}
+
+}
---
+++ demojava/src/demojava/Sree.java
@@ -0,0 +1,11 @@
+package demojava;
+
+public class Sree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String var = new String("Hellow World");
+System.out.println(var);
+	}
+
+}
