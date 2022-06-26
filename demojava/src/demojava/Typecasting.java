f89b76e588c44244b03d61dcb6ee3365558f016c
javva <javva@ANASURYA>
2022-06-26 19:54:29 +0530

    typecasting

--- demojava/src/demojava/Typecasting.java
+++ demojava/src/demojava/Typecasting.java
@@ -4,7 +4,7 @@ public class Typecasting {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int i= 30;//variable with f is a type of which is holding of 30
+int i= 10;//variable with f is a type of which is holding of 30
 long l = (long ) i;//defining avar with name l which of datatype long = covnvert value that is present in l to long
 System.out.println(l);
 
