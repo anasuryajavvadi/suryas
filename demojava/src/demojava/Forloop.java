e915c97ed76d110f0fa2f6999ebfa1380bf27d85
javva <javva@ANASURYA>
2022-06-27 00:18:49 +0530

    forloop

---
+++ demojava/src/demojava/Forloop.java
@@ -0,0 +1,23 @@
+package demojava;
+
+public class Forloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub  
+		{
+			for(int i = 1;i<=10;i++)
+			{
+				System.out.print("value of i:");
+				System.out.println(i);
+				
+				
+				
+				
+				
+			}
+				
+		}
+
+	}
+
+}
