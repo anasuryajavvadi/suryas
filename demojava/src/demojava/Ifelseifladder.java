fe3870d7c5d9e18435d4ba73f575ada50bf2cb21
javva <javva@ANASURYA>
2022-06-27 12:46:42 +0530

    ifelseifladder

---
+++ demojava/src/demojava/Ifelseifladder.java
@@ -0,0 +1,19 @@
+package demojava;
+
+public class Ifelseifladder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String city = "achanta";  
+		if(city == "Meerut") {  
+		System.out.println("city is tpalem");  
+		}else if (city == "Noida") {  
+		System.out.println("city is palkol");  
+		}else if(city == "Agra") {  
+		System.out.println("city is bimavaram");  
+		}else {  
+		System.out.println(city);  
+		}  
+	}
+
+}
