cebe65e8c11825aac39212a2b074b9b974cd9d31
javva <javva@ANASURYA>
2022-07-10 12:52:07 +0530

    swapping

---
+++ demojava/src/demojava/Demoswapping.java
@@ -0,0 +1,22 @@
+package demojava;
+
+public class Demoswapping {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 50,y = 200;
+		System.out.println("Before swap");
+		System.out.println("x= "+x);
+		System.out.println("y=" +y);
+		int temp = x;
+		x = y;
+		y = temp;
+		System.out.println("After swap");
+		System.out.println("x = "+x);
+		System.out.println("y = "+y);
+
+	
+
+	}
+
+}
